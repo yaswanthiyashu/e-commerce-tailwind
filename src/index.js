61f964dd6ebe1f1fe7d8f6592c6a8ec359768037
yaswanthiyashu <107607008+yaswanthiyashu@users.noreply.github.com>
2024-01-08 16:10:43 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import ProductProvider from './Contexts/ProductContext';
+import SidebarProvider from './Contexts/SidebarContext';
+import CartProvider from './Contexts/CartContext';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <SidebarProvider>
+    <CartProvider>
+      <ProductProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ProductProvider>
+    </CartProvider>
+  </SidebarProvider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
